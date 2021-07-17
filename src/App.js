0fe74036bf4774a2ccf91fbda9ba6ba2d3de8d23
Gary Smith <goose200782@gmail.com>
2021-07-17 15:02:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take the rubbish out']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <input />
+      <button>Add To Do</button>
+      
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+        
+      </ul>
     </div>
   );
 }
