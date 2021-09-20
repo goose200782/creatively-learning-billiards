58472bd828dbbf3f626c440d869b898eb42e1a89
Gary Smith <goose200782@gmail.com>
2021-09-20 11:55:29 -0500

    Added structure for React Router and created Header2.js. Also imported ScrollTop component

--- src/App2.js
+++ src/App2.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App2.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ScrollToTop from './ScrollToTop';
+import Header2 from './Header2';
+
 
 function App2() {
     return (
         <div>
-            
+            <Router>
+                <ScrollToTop />
+                <Header2 />
+                <Switch>
+                    <Route path="/">
+                        
+                    </Route>
+                </Switch>
+                
+            </Router>
         </div>
     )
 }
---
+++ src/Header2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header2() {
+    return (
+        <div>
+            <h1>I will be the header</h1>
+        </div>
+    )
+}
+
+export default Header2
