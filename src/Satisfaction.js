9511a4cfd0522cabdc31e5a3155d17d207c101bf
Gary Smith <goose200782@gmail.com>
2021-09-21 18:42:35 -0500

    Finished satisfaction page. Fixed expanding icons. Started on Picture grid

--- src/Satisfaction.js
+++ src/Satisfaction.js
@@ -1,43 +1,73 @@
-import React,  { useEffect, useState } from 'react';
+import React,  { useEffect ,useState } from 'react';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 function Satisfaction() {
-    const [icon,setIcon] = useState(()=>(
-        <span className='red big bold' id='iconSpan'>
-            <ExpandMoreIcon id='icon'/>
-        </span>));
-
     useEffect(() => {
         var coll = document.getElementsByClassName("collapsible");
         var i;
 
         for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-                content.style.maxHeight = null;
-                setIcon(()=>(
-                    <span className='red big bold' id='iconSpan'>
-                        <ExpandMoreIcon id='icon'/>
-                    </span>))
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-                setIcon(()=>(
-                    <span className='red big bold' id='iconSpan'>
-                        <ExpandLessIcon id='icon'/>
-                    </span>))
-            } 
-        });
+            coll[i].addEventListener("click", function() {
+                var num = this.getAttribute('id').match(/\d/g).join("");
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.maxHeight){
+                    content.style.maxHeight = null;
+                    switch (num) {
+                        case '1':
+                            setIcon1(more)
+                            break;
+                        case '2':
+                            setIcon2(more)
+                            break;
+                        case '3':
+                            setIcon3(more)
+                            break;
+                    
+                        default:
+                            break;
+                    }
+                    
+                } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                    console.log("number: ", num)
+                    switch (num) {
+                        case '1':
+                            setIcon1(less)
+                            break;
+                        case '2':
+                            setIcon2(less)
+                            break;
+                        case '3':
+                            setIcon3(less)
+                            break;
+                    
+                        default:
+                            break;
+                    }
+                    
+                } 
+            });
         }
     }, [])
 
+    const more = () =>(
+        <span className='red big bold' id='iconSpan1'>
+            <ExpandMoreIcon id='icon1'/>
+        </span>)
+
+    const less = () =>(
+        <span className='green big bold' id='iconSpan1'>
+            <ExpandLessIcon id='icon1'/>
+        </span>)
+
+    const [icon1, setIcon1] = useState(more);
+    const [icon2, setIcon2] = useState(more);
+    const [icon3, setIcon3] = useState(more);
+
     
-    const click1 = (event) => {
-        
-        
-    }
+
 
     return (
         <div className='satisfaction top font black alignCenter borderRadius'>
@@ -50,19 +80,19 @@ function Satisfaction() {
             </div>   
             <ul className='right flexCol'>
                 <li>
-                    <button id='collapse1' className="collapsible borderRadiusTop" onClick={click1}>{icon} Illuminated Cueing Arts Training System</button>
+                    <button id='collapse1' className="collapsible borderRadiusTop">{icon1} Illuminated Cueing Arts Training System</button>
                     <div className="content borderRadiusBottom">
                         <p className='small'>We offer a full spectrum of professionally prepared modules utilizing the ICA training system to enhance your performance and skills.</p>
                     </div>
                 </li>
                 <li>
-                    <button id='collapse2' className="collapsible borderRadiusTop">{icon} Mobile and Zoom lessons available</button>
+                    <button id='collapse2' className="collapsible borderRadiusTop">{icon2} Mobile and Zoom lessons available</button>
                     <div className="content borderRadiusBottom">
                         <p className='small'>Connect with us locally in the general New Orleans area or online via Zoom.</p>
                     </div>
                 </li>
                 <li>
-                    <button id='collapse3' className="collapsible borderRadiusTop">{icon} Lessons for all skill levels</button>
+                    <button id='collapse3' className="collapsible borderRadiusTop">{icon3} Lessons for all skill levels</button>
                     <div className="content borderRadiusBottom">
                         <p className='small'>Customized lessons for kids, amateurs and pro level players.</p>
                     </div>
